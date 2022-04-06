b8ad3d1d39a406027bab484e54070cff36a68017
youhualu <102488793+youhualu@users.noreply.github.com>
2022-04-05 22:59:13 -0500

    HW2

---
+++ HW2_LU.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=HW2_LU.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/HW2_LU.X.production.hex
